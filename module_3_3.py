6f3991d86f2b95e8e0b17eb586facb5dade02482
tmukhamedjanov <t.mukhamedjanov@mail.ru>
2024-10-07 04:05:22 +0300

    7

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [1, "Test", True]
+values_dict = {'a' : 4, "b" : "Slovar", "c" : 3.14}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
